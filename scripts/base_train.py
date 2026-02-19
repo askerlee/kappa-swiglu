174483a6458c6782cddef78d3042811ba99000dc
askerlee <shaohua@gmail.com>
2026-02-19 14:50:52 +0800

    Add --router-ortho-neg-corr-weight argument for weighting negative correlations in router orthogonality loss.

--- scripts/base_train.py
+++ scripts/base_train.py
@@ -138,6 +138,7 @@ parser.add_argument("--moe-top-k", type=int, default=2, help="top-k of the MoE r
 parser.add_argument("--router-ortho-loss-weight", type=float, default=0.001, help="weight for router orthogonality loss")
 parser.add_argument("--router-ortho-loss-leave-one-out", type=str2bool, nargs='?', const=True, default=False, help="whether to leave one dimension out of the router orthogonality loss.")
 parser.add_argument("--router-ortho-loss-grad-scale", type=float, default=1.0, help="scaling factor for gradients to expert gate projection weights during router orthogonality loss computation. Setting this to a value < 1.0 allows adjusting expert weights slightly, without hurting representation learning too much.")
+parser.add_argument("--router-ortho-neg-corr-weight", type=float, default=1.0, help="weight for negative correlations in router-ortho loss.")
 parser.add_argument("--gate-output-loss-weight", type=float, default=0.0001, help="weight for gate output regularization loss")
 parser.add_argument("--experts-ortho-loss-weight", type=float, default=0.01, help="weight for experts orthogonality loss")
 parser.add_argument("--router-z-loss-weight", type=float, default=0.00001, help="weight for router z loss")
@@ -260,6 +261,7 @@ def build_model_meta(depth):
         n_layer=depth, moe_start_layer=args.moe_start_layer,
         n_exp=args.n_exp, moe_top_k=args.moe_top_k,
         router_ortho_loss_weight=args.router_ortho_loss_weight,
+        router_ortho_neg_corr_weight=args.router_ortho_neg_corr_weight,
         router_ortho_loss_leave_one_out=args.router_ortho_loss_leave_one_out,
         # this is the alpha in the paper that scales down gradients to expert gate projection weights during router orthogonality loss computation.
         router_ortho_loss_grad_scale=args.router_ortho_loss_grad_scale,  
